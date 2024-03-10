6c31f9e9754d472484dd837d65528d5087f474f4
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2024-03-10 12:49:40 -0400

    correccion manager

--- practica1.cpp
+++ practica1.cpp
@@ -2,24 +2,39 @@ const int PIN_GREEN = 12;
 const int PIN_YELLOW = 14;
 const int PIN_RED = 27;
 const long MIN_DISTANCE = 2;
-const long MAX_DISTANCE = 400;
+const long MAX_DISTANCE = 100;
+
+
+
 
 class LED {
   private:
     int pin;
+
+
+
 
   public:
     LED(int pin) : pin(pin) {
       pinMode(pin, OUTPUT);
     }
 
+
+
+
     void turnOn() {
       digitalWrite(pin, HIGH);
     }
 
+
+
+
     void turnOff() {
       digitalWrite(pin, LOW);
     }
+
+
+
 
     void blink() {
       turnOn();
@@ -29,15 +44,24 @@ class LED {
     }
 };
 
+
+
+
 class UltrasonicSensor {
   private:
     int triggerPin;
     int echoPin;
 
+
+
+
   public:
     UltrasonicSensor(int triggerPin, int echoPin) : triggerPin(triggerPin), echoPin(echoPin) {}
 
-    long readUltrasonicDistance() {
+
+
+
+    double readUltrasonicDistance() {
       pinMode(triggerPin, OUTPUT);  // Clear the trigger
       digitalWrite(triggerPin, LOW);
       delayMicroseconds(2);
@@ -52,32 +76,54 @@ class UltrasonicSensor {
 };
 
 
-void manageLEDs(long distance, LED& red, LED& yellow, LED& green) {
-  red.turnOff();
-  yellow.turnOff();
-  green.turnOff();
 
-  if (distance >= MIN_DISTANCE &&  distance <= 20) {
+
+
+
+
+
+void manageLEDs(long distance, LED& red, LED& yellow, LED& green) {
+  if (distance >= MIN_DISTANCE &&  distance <= 15) {
     red.blink();
-  } else if (distance > 20 && distance <= 132) {
+    yellow.turnOff();
+    green.turnOff();
+  } else if (distance > 15 && distance <= 43) {
     red.turnOn();
-  } else if (distance > 132 && distance <= 265) {
+    yellow.turnOff();
+    green.turnOff();
+  } else if (distance > 43 && distance <= 71) {
+    red.turnOff();
     yellow.turnOn();
-  } else if (distance > 265 && distance <= MAX_DISTANCE) {
+    green.turnOff();
+  } else if (distance > 71 && distance <= MAX_DISTANCE) {
+    red.turnOff();
+    yellow.turnOff();
     green.turnOn();
   }
+  else
+  {
+    red.turnOff();
+    yellow.turnOff();
+    green.turnOff();
+  }
 }
+
+
+
 
 void setup() {
   Serial.begin(9600);
 }
 
+
+
+
 void loop() {
   UltrasonicSensor sensor(25, 26);
   LED red(PIN_RED), yellow(PIN_YELLOW), green(PIN_GREEN);
-  long distance = sensor.readUltrasonicDistance();
-
+  double distance = sensor.readUltrasonicDistance();
+  Serial.print(distance);
+  Serial.println("cm");
+  delay(100); // Wait for 100 millisecond(s)
   manageLEDs(distance, red, yellow, green);
-
-  delay(400);
 }
